145e0d116ede635bb388a55d957e48874a64ccbe
Tejas Gawade <tgawade092@gmail.com>
2024-11-17 17:41:03 +0530

    Add videoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Navbar from "./components/Navbar"
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import VideoForm from "./components/VideoForm";
 
 function App() {
+  const [videoInfo, setVideoInfo] = useState(null);
 
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-        <Navbar />
+          <Navbar />        
+        <div className="w-full h-full flex justify-center p-4 mt-10">
+          <VideoForm setVideoInfo={setVideoInfo}/>          
+        </div>
       </div>
     </>
   )
---
+++ src/components/VideoForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const VideoForm = ({setVideoInfo}) => {
+    const [url, setUrl] = useState("");
+
+    const fetchVideoInfo = async() => {
+        try {
+            const res = await axios.post("http://localhost:3000/api/video-info", { url }, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded', //x-www-form-encoding
+                }
+            })
+            setVideoInfo(res.data);
+        } catch (error) {
+            console.log("Error fetching video info: ", error);
+            alert("Failed to fetch video info, please try again");
+        }
+    }
+
+  return (
+    <div className=' w-2/4 flex items-center gap-2 h-fit'>
+        <input 
+        type="text"
+        className='border border-black rounded-md px-2 py-2 w-[80%] h-12'
+        placeholder='Paste youtube video url here'
+        value={url}
+        onChange={(e) => setUrl(e.target.value)} 
+        />
+        <button
+        onClick={fetchVideoInfo}
+        className='bg-blue-700 text-white px-2 py-2 w-fit h-12 rounded-md'
+        >
+            Fetch video info
+        </button>
+    </div>
+  )
+}
+
+export default VideoForm
